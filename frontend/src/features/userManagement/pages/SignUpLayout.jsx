f0d746950990b32c969d09434b9a9705b4a99221
wahidddhrubo <wahiddhrubo@gmail.com>
2025-01-03 20:48:31 +0600

    Refactor Pages

---
+++ frontend/src/features/userManagement/pages/SignUpLayout.jsx
@@ -0,0 +1,19 @@
+import Footer from "../../../shared/components/footer/Footer";
+import Navbar from "../../../shared/components/navbar/Navbar";
+import RegisterForm from "../components/registerForm/RegistrationForm";
+
+const SignUpLayout = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <div className="mt-10">
+        {" "}
+        {/* Add margin-top */}
+        <RegisterForm />
+      </div>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default SignUpLayout;
